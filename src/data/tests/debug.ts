82be3b295a025f4dc06fba733fcc7ae26619aa44
零泠丶 <632177447@qq.com>
2026-04-11 01:56:36 +0800

    优化测试描述文案并清理生产环境冗余资源

--- src/data/tests/debug.ts
+++ src/data/tests/debug.ts
@@ -47,7 +47,7 @@ export const debugTest: TestSuite = {
       id: 'debug_master',
       title: '调试大师',
       subtitle: 'Debug Master',
-      image: 'visionary.png',
+    //   image: 'visionary.png',
       emoji: '🧙‍♂️',
       color: '#88D8C0',
       description: [
