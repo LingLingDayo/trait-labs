af4649c4536ef87d4b7731c41b74275efb3d1c10
零泠丶 <632177447@qq.com>
2026-04-10 23:44:50 +0800

    配置 GitHub Pages 自动部署工作流并优化动态构建路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 判断是否在 GitHub Actions 环境中运行
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  // 如果是 GitHub 自动化部署，就用仓库路径；本地打包则保留 ./
+  base: isGitHubActions ? '/trait-labs/' : './',
   plugins: [vue()],
   server: {
     host: true,
